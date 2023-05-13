0e139a9d3c656fbd15ac44cea62a77a250b73848
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-05-13 11:00:48 -0700

    added inquirer for adding a department and adding a role to the database

--- index.js
+++ index.js
@@ -2,59 +2,81 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const fs = require('fs');
 const mysql = require('mysql2');
+const db = require('./connection')
 
+db.connect (function(err,results) {
+    if(err) {
+        console.log(err);
+    } 
+    employee_data();
+});
 
-console.table([
-    {
-      name: 'foo',
-      age: 10
-    }, {
-      name: 'bar',
-      age: 20
-    }
-  ]);
-
+var employee_data = function() {
 inquirer
     .prompt([
         {
             type: 'list',
             name: 'Home',
             message: 'What would you like to do?',
-            choices: ['Update an employee role', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'],
         },
-        /*{
-            type: 'list',
-            name: 'Home6',
-            message: 'What would you like to do?',
-            choices: ['Uj', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'],
-            when: (data) => data.Home === 'Update an employee role',
-        },*/
     ])
 
+
     .then((data) => {
-        if (data.Home === 'Update an employee role') {
-            const db = mysql.createConnection(
-            {
-                host: 'localhost',
-                user: 'root',
-                password: 'Caramel505',
-                database: 'company_db'
-            },
-
-            console.log(`Connected to the company_db database.`)
-            );
-        
-            /*
-            db.query('source schema.sql;', function(err,results) {
-                console.log(results);
+        if(data.Home === 'View all departments'){
+            db.query('SELECT * FROM department', function(err,results) {
+                console.table(results);
+                employee_data();
             });
-            
-            db.query('source seeds.sql;', function(err,results) {
-                console.log(results);
-            });*/
-
+        } else if (data.Home === 'View all roles') {
+            db.query('SELECT * FROM roles', function(err,results) {
+                console.table(results);
+                employee_data();
+            });
+        } else if (data.Home === 'View all employees') {
             db.query('SELECT * FROM employee', function(err,results) {
                 console.table(results);
+                employee_data();
             });
+        } else if (data.Home === 'Add a department') {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'Department',
+                        message: 'Add the name of your department: ',
+                    }
+                ]).then((data) => {
+                    db.query('INSERT INTO department (id, name) VALUES ()', [data.Department],  function(err,results){
+                        employee_data();
+                    });
+                })
+        } else if (data.Home === 'Add a role') {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'Role_Name',
+                        message: 'Add the name of your new role: ',
+                    },
+                    {
+                        type: 'input',
+                        name: 'Salary',
+                        message: 'Add the salary for this role: ',
+                    },
+                    {
+                        type: 'input',
+                        name: 'Role_Department',
+                        message: 'Add the department for this role: ',
+                    }
+                ]).then((data) => {
+                    db.query('INSERT INTO roles (id, title, salary, department_id) VALUES ()',  [data.Department],  function(err,results){
+                        employee_data();
+                    });
+                })
         }
     });
+}
+
+
